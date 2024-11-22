a052b8995f1dc998cf38a9a190d68e787458202d
Byran Murphy <bryanmurphy401@gmail.com>
2024-11-22 14:22:19 +0000

    Created an about app and reverted debug to false to deploy on heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-upvg4!)dmky@qu-x*ry()((h$&&wkgua(83=!t7s&z0g==8^zl
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-chipsandbea-djangoblog1-ghagcdqq0bc.ws.codeinstitute-ide.net', 
